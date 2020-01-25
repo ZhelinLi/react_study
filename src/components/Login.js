5a040523267725fab9cea4a7be1318b6faf5d4ec
Zhelin Li <52447754+ZhelinLi@users.noreply.github.com>
2020-01-25 15:22:38 -0800

    initialize

---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+class Login extends React.Component{
+    render(){
+        return <form>
+            <label>
+                Name:
+                <input type="text" name="name"/>
+            </label>
+            <label>
+                Name:
+                <input type="text" name="hshs"/>
+            </label>
+            <input type="submit" value="haha" />
+        </form>
+    }
+    
+}
+
+export default Login;
